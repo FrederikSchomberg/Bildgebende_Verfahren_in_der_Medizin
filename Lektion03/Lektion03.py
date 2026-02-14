89442354339022e3ae4f98e830662aab984e83f0
Frederik Schomberg <schomberg.f@web.de>
2026-02-14 22:48:35 +0100

    read methode added

--- Lektion03/Lektion03.py
+++ Lektion03/Lektion03.py
@@ -4,31 +4,37 @@ import cv2
 import matplotlib.pyplot as plt
 import skimage as ski
     
-#2.1
+def _read_gray(fname: str):
+    I = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
+    if I is None:
+        print(f"Fehler: '{fname}' konnte nicht gelesen werden.")
+    return I  
+    
+#3.1
 def a1():
     print("-----------Aufgabe 3.1-----------")
 
-#2.2
+#3.2
 def a2():
     print("-----------Aufgabe 3.2-----------")
 
-#2.3
+#3.3
 def a3():
     print("-----------Aufgabe 3.3-----------")
 
-#2.4
+#3.4
 def a4():
     print("-----------Aufgabe 3.4-----------")
 
-#2.5
+#3.5
 def a5():
     print("-----------Aufgabe 3.5-----------")
 
-#2.6
+#3.6
 def a6():
     print("-----------Aufgabe 3.6-----------")
 
-#2.7
+#3.7
 def a7():
     print("-----------Aufgabe 3.7-----------")
     
